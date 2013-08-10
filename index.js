47dd5c925801a06ec5c455c80c22c7732d3ee908
Shallker Wang <shallker.wang@profero.com>
2013-08-10 15:25:15 +0800

    Rename 'node-serve' to 'start-up'.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib/node-serve');
+var startup = require('./lib/start-up');
+
+exports.version = '1.0.0';
+
+exports.createProject = function(name) {
+  startup.createProject(name);
+}
+
+exports.createConfig = function() {
+  startup.createConfig();
+}
+
+exports.serve = function() {
+  startup.serve();
+}
